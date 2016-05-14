76c93d79e0d8a9410a84faaaf301ebe3fca4e8b4
codloverS2 <mirelle.bueno21@hotmail.com>
2016-05-14 16:22:18 -0300

    Att

---
+++ Sistema Calculo médias-Codificação/Unit1.pas
@@ -0,0 +1,55 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, jpeg, ExtCtrls, StdCtrls, ComCtrls, XPMan;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Timer1: TTimer;
+    ProgressBar1: TProgressBar;
+    XPManifest1: TXPManifest;
+    Label2: TLabel;
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+progressbar1.Position:= 0;
+
+while progressBar1.Position < 100 do
+begin
+ Progressbar1.Position:=Progressbar1.Position +10;
+ case Progressbar1.Position of
+ 10:label2.caption:='Carregando.....';
+ 20:label2.caption:='Abrindo Arquivos.......';
+ 30: label2.Caption:='Inializando o Programa.......';
+ end;
+ form1.Update;
+ sleep(500);
+ end;
+
+ form1.Update;
+ sleep(200);
+
+form1.Destroy;
+//form2.show;
+
+end;
+
+end.
