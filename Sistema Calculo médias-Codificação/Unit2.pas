73a3ddd5d68b8066464ce353c2f2add2ec75f139
codloverS2 <mirelle.bueno21@hotmail.com>
2016-05-20 11:22:38 -0300

    att

--- Sistema Calculo médias-Codificação/Unit2.pas
+++ Sistema Calculo médias-Codificação/Unit2.pas
@@ -5,28 +5,16 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, TFlatButtonUnit, jpeg, ExtCtrls, TFlatEditUnit,
-  TFlatGroupBoxUnit,pngimage;
+  TFlatGroupBoxUnit;
 
 type
   TForm2 = class(TForm)
-    Image1: TImage;
     FlatButton1: TFlatButton;
     FlatButton2: TFlatButton;
     FlatButton3: TFlatButton;
     FlatButton4: TFlatButton;
     Memo1: TMemo;
-    FlatButton5: TFlatButton;
-    FlatButton6: TFlatButton;
-    FlatButton7: TFlatButton;
-    FlatButton8: TFlatButton;
-    FlatButton9: TFlatButton;
-    FlatButton10: TFlatButton;
-    FlatButton11: TFlatButton;
-    FlatButton12: TFlatButton;
-    FlatButton13: TFlatButton;
-    FlatButton14: TFlatButton;
-    Image2: TImage;
-    FlatButton15: TFlatButton;
+    Image1: TImage;
   private
     { Private declarations }
   public
